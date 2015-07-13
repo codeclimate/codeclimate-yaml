6ec81094e657de6724504b7fdf5e64010ade6a01
ABaldwinHunter <abaldwinhunter@codeclimate.com>
2015-07-13 16:21:27 -0400

    update expected format of languages key in yaml. update tests

--- lib/cc/yaml/nodes/language_list.rb
+++ lib/cc/yaml/nodes/language_list.rb
@@ -1,8 +1,10 @@
 module CC
   module Yaml
     module Nodes
-      class LanguageList < Sequence
-        type Language
+      class LanguageList < Mapping
+        %w[Ruby JavaScript Python PHP].each do |language|
+          map :"#{language}", to: Scalar[:bool], required: false
+        end
       end
     end
   end
