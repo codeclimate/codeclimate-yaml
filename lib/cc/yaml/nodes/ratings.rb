09d2cfcc77e948401cb05fc1938030b10a522b59
ABaldwinHunter <abaldwinhunter@codeclimate.com>
2015-07-23 12:02:02 -0400

    remove code related to exclude? and rate? checks

--- lib/cc/yaml/nodes/ratings.rb
+++ lib/cc/yaml/nodes/ratings.rb
@@ -3,10 +3,6 @@ module CC
     module Nodes
       class Ratings < Mapping
         map :paths, to: GlobList
-
-        def rate?(path)
-          paths.present? && paths.match_any?(path)
-        end
       end
     end
   end
