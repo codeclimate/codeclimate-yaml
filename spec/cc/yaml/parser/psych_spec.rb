4f3ae59576c7367b0f8316ebeb08ca18d4bbe359
Jacek Kubiak <jacek.kubiak@netguru.pl>
2015-09-09 12:30:18 +0200

    Add failing spec

--- spec/cc/yaml/parser/psych_spec.rb
+++ spec/cc/yaml/parser/psych_spec.rb
@@ -7,7 +7,47 @@ describe CC::Yaml::Parser::Psych do
       - "*.rb"
       - "test/*"
     YAML
+
     config = CC::Yaml.parse yaml
     config.errors.must_include CC::Yaml::Parser::Psych::NO_ANALYSIS_KEY_FOUND_ERROR
+  end
+
+  specify "properly parses engine data" do
+    yaml = <<-YAML
+    ---
+    engines:
+      duplication:
+        enabled: true
+        config:
+          ruby:
+            mass_threshold: 10
+          javascript:
+            mass_threshold: 20
+    YAML
+
+    parsed_yaml = CC::Yaml.parse yaml
+    config = parsed_yaml["engines"]["duplication"]["config"]
+
+    assert config.empty?, false
+    assert_equal config["ruby"]["mass_threshold"], 10
+    assert_equal config["javascript"]["mass_threshold"], 20
+  end
+
+  specify "supports listed values" do
+    yaml = <<-YAML
+    ---
+    engines:
+      duplication:
+        enabled: true
+        languages:
+          - "ruby"
+          - "javascript"
+    YAML
+
+    parsed_yaml = CC::Yaml.parse yaml
+    enabled_languages = parsed_yaml["engines"]["duplication"]["languages"]
+
+    assert enabled_languages.nil? false
+    assert_equal enabled_languages, ["ruby", "javascript"]
   end
 end
