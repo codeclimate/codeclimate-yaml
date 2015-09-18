ebd5d16b221684c7a09bd4be4d003e6d0cd823b7
Jacek Kubiak <jacek.kubiak@netguru.pl>
2015-09-18 15:49:18 +0200

    Update the psych parser spec

--- spec/cc/yaml/parser/psych_spec.rb
+++ spec/cc/yaml/parser/psych_spec.rb
@@ -14,7 +14,6 @@ describe CC::Yaml::Parser::Psych do
 
   specify "properly parses engine data" do
     yaml = <<-YAML
-    ---
     engines:
       duplication:
         enabled: true
@@ -26,28 +25,28 @@ describe CC::Yaml::Parser::Psych do
     YAML
 
     parsed_yaml = CC::Yaml.parse yaml
-    config = parsed_yaml["engines"]["duplication"]["config"]
+    config = parsed_yaml["engines"]["duplication"].config
 
-    assert config.empty?, false
+    assert !config.empty?
     assert_equal config["ruby"]["mass_threshold"], 10
     assert_equal config["javascript"]["mass_threshold"], 20
   end
 
   specify "supports listed values" do
     yaml = <<-YAML
-    ---
     engines:
       duplication:
         enabled: true
-        languages:
-          - "ruby"
-          - "javascript"
+        config:
+          languages:
+          - ruby
+          - javascript
     YAML
 
     parsed_yaml = CC::Yaml.parse yaml
-    enabled_languages = parsed_yaml["engines"]["duplication"]["languages"]
+    config = parsed_yaml["engines"]["duplication"].config
 
-    assert enabled_languages.nil? false
-    assert_equal enabled_languages, ["ruby", "javascript"]
+    assert !config['languages'].nil?
+    assert_equal config['languages'], ["ruby", "javascript"]
   end
 end
