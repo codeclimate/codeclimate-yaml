70762ffcafd51f1b6b436b32da676abaed7e1ce4
patrick brisbin <pbrisbin@gmail.com>
2016-05-24 17:18:55 -0400

    Add engine.channel attribute
    - Will be used to select alternate engine images via config
    - Defaults to "stable"
    
    Note: I tried _extremely_ hard to get the supposedly built-in method of
    defaulting values to work. It does not. The code we cargo-culted from
    travis-cs/travis-yaml is old and bad.

--- lib/cc/yaml/nodes/engine.rb
+++ lib/cc/yaml/nodes/engine.rb
@@ -2,11 +2,18 @@ module CC
   module Yaml
     module Nodes
       class Engine < Mapping
+        DEFAULT_CHANNEL = "stable".freeze
+
         map :enabled, to: Scalar[:bool], required: true
+        map :channel, to: Scalar
         map :checks, to: Checks
         map :config, to: EngineConfig
         map :exclude_fingerprints, to: Sequence
         map :exclude_paths, to: GlobList
+
+        def channel
+          self["channel"] || DEFAULT_CHANNEL
+        end
       end
     end
   end
